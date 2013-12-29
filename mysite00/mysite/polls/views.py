4c5567fb215a29d4ff9bb3f0f3a0876eabfa7c5b
Jin Lee <shadowkxjin@gmail.com>
2013-12-28 17:28:56 -0700

    templating, url routing and views

--- mysite00/mysite/polls/views.py
+++ mysite00/mysite/polls/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.http import Http404
+from django.template import RequestContext, loader
+
+from polls.models import Poll
 
 # Create your views here.
+
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+def results(request, poll_id):
+    return HttpResponse("you're looking at the results of poll %s" % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("you're voting on poll %s" % poll_id)
+    
